6b9172ed8345e0287e6a20b09eeed6740afe8164
straubt1 <tstraub@hashicorp.com>
2024-01-10 14:29:12 -0600

    sort and format

--- generate_settings_from_registry.sh
+++ generate_settings_from_registry.sh
@@ -5,8 +5,8 @@ gen_json(){
 namespace=${1:-"hashicorp"}
 provider=${2:-"aws"}
 echo "generate version json files for namespace: $namespace, provider: $provider"
-v=$(curl -s "$reg/$namespace/$provider/versions" -q|jq  '[.versions[].version]')
- cat > ${namespace}-${provider}.json << EOF
+v=$(curl -s "$reg/$namespace/$provider/versions" -q|jq  '[.versions[].version] | sort_by(.)')
+ jq > ${namespace}-${provider}.json << EOF
 {
     "providers": [{
             "namespace": "$namespace",
